68e80f6fb175fe3b933739213603752c41a38ca6
x71 <x@mbp.com>
2021-12-28 20:46:08 +0100

    changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+alias q="exit"
+alias paru="paru --bottomup"
+
 set fish_greeting "🐟"
 
 starship init fish | source
 
-#source ~/.alias
+source ~/.alias
 
+
+thefuck --alias | source
