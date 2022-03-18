0801f86f8d6df0ff138de64c6c0a62650c2101bc
xkdots <xavktsky@gmail.com>
2022-03-18 02:49:27 +0100

    changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,28 +17,56 @@ starship init fish | source
 
 source ~/.alias
 
+
+# key remaps works in kitty
+# ctrl backspace
+bind \ch backward-kill-word
+# ctrl del
+bind '[3;5~' kill-word
+# shift up
+bind '[1;2A' up
+# shift down
+bind '[1;2B' down
+# ctrl up
+bind '[1;5A' any
+# ctrl down
+bind '[1;5B' go
+
 function up
     cd ..
-    pwd
+    echo ❯ (pwd)
+    ls
 end
 
 function down
-    cd (fd -t d | fzf)
-    pwd
+    set adir (fd -t d --maxdepth 1 | fzf)
+    if test -n "$adir"
+        cd $adir
+    end
+    echo ❯ (pwd)
+    ls
 end
 
 function go
-    cd (fd -t d --maxdepth 1 | fzf)
-    pwd
+    set bdir (fd -t d | fzf)
+    if test -n "$bdir"
+        cd $bdir
+    end
+    echo ❯ (pwd)
+    ls
 end
 
-# key remaps works in kitty
-bind \ch backward-kill-word
-bind '[3;5~' kill-word
-bind '[1;2A' up
-bind '[1;2B' down
-bind '[1;5A' any
-bind '[1;5B' go
+function any
+    set any (locate / | fzf --preview 'bat --style numbers,changes --color=always {} | head -500' )
+    if test -n "$any"
+        vim $any
+    end
+    # TODO key bind or universal to open
+    # fzf --bind "enter:execute(less {})"
+    # https://gist.github.com/mb720/86144b670599c0eab331cd2f48bd23b9
+    # TODO make a keybind to open shell in current file directory
+    # TODO ripgrep with preview
+end
 
 function loc
     if count $argv > /dev/null
@@ -46,15 +74,6 @@ function loc
     else
         locate / | fzf --preview 'bat --style numbers,changes --color=always {} | head -500'  | xclip
     end
-end
-
-function any
-    # TODO key bind or universal to open
-    # fzf --bind "enter:execute(less {})"
-    # https://gist.github.com/mb720/86144b670599c0eab331cd2f48bd23b9
-    vim (locate / | fzf --preview 'bat --style numbers,changes --color=always {} | head -500' )
-    # TODO make a keybind to open shell in current file directory
-    # TODO ripgrep with preview
 end
 
 function m
